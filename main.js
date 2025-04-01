2ead9f130a387e5452c9cd7bcaf015aacfa98481
Warren <warren.r.davison@gmail.com>
2025-04-01 12:04:57 -0400

    has bookmarks and time slider implemented

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // Define the map and bookmarks components
 const mapElement = document.querySelector("arcgis-map");
 const bookmarksElement = document.querySelector("arcgis-bookmarks");
+const timeSlider = document.querySelector("arcgis-time-slider");
 
 // Define a the mapping between slides and time ranges
 const choreographyMapping = {
@@ -13,6 +14,7 @@ mapElement.addEventListener("arcgisViewReadyChange", (event) => {
   if (event.target.ready) {
     const view = mapElement.view; // Access the MapView from the arcgis-map component
     const map = view.map; // Access the WebMap instance from the view
+    
 
     // Access the layers in the map
     const layers = map.layers;
@@ -27,8 +29,12 @@ mapElement.addEventListener("arcgisViewReadyChange", (event) => {
         console.log(targetLayer.title, "is now visible.");
         // Apply the track renderer
         targetLayer.timeInfo = {
-          startField: "startTimeField",
-          trackIdField: "tag_local_identifier"
+          startField: "timestamp",
+          trackIdField: "tag_local_identifier",
+          interval: {
+            unit: "hours", // set time interval to one hour
+            value: 1
+          }
         };
         targetLayer.trackInfo = {
           enabled: true,
@@ -40,21 +46,21 @@ mapElement.addEventListener("arcgisViewReadyChange", (event) => {
               symbol: {
                 type: "simple-marker",
                 style: "circle",
-                color: "red",
+                color: "White",
                 size: 10
               }
             }
           },
           previousObservations: {
-            enabled: false,
-            visible: false,
+            enabled: true,
+            visible: true,
             renderer: {
               type: "simple",
               symbol: {
                 type: "simple-marker",
                 style: "circle",
                 color: "white",
-                size: 2
+                size: 3
               }
             }
           },
@@ -66,66 +72,61 @@ mapElement.addEventListener("arcgisViewReadyChange", (event) => {
               symbol: {
                 type: "simple-line",
                 color: "black",
-                width: 6
+                width: 4
               }
             }
           }
         }
       // 
       // Define the time slider component
-      const timeSlider = document.querySelector("arcgis-time-slider");
-          // Define function to update the choreography based on the hash
-          function updateChoreographyFromHash() {
-            const hash = window.location.hash;
-            console.log(`Hash: ${hash}`);
-            if (choreographyMapping[hash]) {
-              // Set the initial map extent by the bookmarkStart
-              const bookmark = choreographyMapping[hash].bookmark;
-              const bookmarks = Array.from(bookmarksElement.bookmarks);
-              const targetBookmark = bookmarks.find(b => b.name === bookmark);
+      
+      // Define function to update the choreography based on the hash
+      function updateChoreographyFromHash() {
+        const hash = window.location.hash;
+        console.log(`Hash: ${hash}`);
+        if (choreographyMapping[hash]) {
+          // Set the initial map extent by the bookmarkStart
+          const bookmark = choreographyMapping[hash].bookmark;
+          const bookmarks = Array.from(bookmarksElement.bookmarks);
+          const targetBookmark = bookmarks.find(b => b.name === bookmark);
 
-              // Find the bookmark by name
-              // If the bookmark exists, navigate to it
-              if (targetBookmark) {
-                mapElement.goTo(targetBookmark.viewpoint, { duration: 6000 });  // Navigates to the bookmark view
-              } else {
-                console.error(`Bookmark "${bookmark}" not found!`);
-              } 
+          // Configure the time sliders full extent with the start and end time from choreography
+          const startFrame = new Date(choreographyMapping[hash].start);
+          const endFrame = new Date(choreographyMapping[hash].end);
+          timeSlider.fullTimeExtent = {start: startFrame, end: endFrame};
 
-              // Configure the time sliders full extent with the start and end time from choreography
-              const startFrame = new Date(choreographyMapping[hash].start);
-              const endFrame = new Date(choreographyMapping[hash].end);
-              timeSlider.fullTimeExtent = {start: startFrame, end: endFrame};
-
-              // Set the time slider's stops to 1 day intervals
-              timeSlider.stops = {
-                interval: {
-                  value: 1,
-                  unit: "hours"
-                }
-              };
-
-              // Set the time slider initial time extent
-              let start = startFrame
-              let end = new Date(startFrame.getTime() + 24 * 60 * 60 * 1000);
-              timeSlider.timeExtent = { start, end };
-              
-              // Start a TimeSlider animation if not already playing
-              if (timeSlider.state === "ready") {
-                timeSlider.play();
-              }
+          // Set the time slider's stops to 1 day intervals
+          timeSlider.stops = {
+            interval: {
+              value: 1,
+              unit: "hours"
             }
+          };
+          
+          // Start a TimeSlider animation if not already playing
+          if (timeSlider.state === "ready") {
+            timeSlider.play();
+          }
+
+          // Find the bookmark by name
+          // If the bookmark exists, navigate to it
+          if (targetBookmark) {
+            mapElement.goTo(targetBookmark.viewpoint, { duration: 6000 });  // Navigates to the bookmark view
+          } else {
+            console.error(`Bookmark "${bookmark}" not found!`);
+          } 
         }
-        updateChoreographyFromHash();
+    }
+    updateChoreographyFromHash();
 
-        // Listen for hash changes to update the choreography
-        window.addEventListener("hashchange", updateChoreographyFromHash);
+    // Listen for hash changes to update the choreography
+    window.addEventListener("hashchange", updateChoreographyFromHash);
 
 
 
-      }).catch((error) => {
-        console.error("Error loading the layer:", error);
-      });
+    }).catch((error) => {
+      console.error("Error loading the layer:", error);
+    });
     } else {
       console.log("Layer not found.");
     }
